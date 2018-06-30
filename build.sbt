aa4e2811839f02f1412f22ef83f8b44f7fd63352
Kwon, Yeong-Eon <eirny@uangel.com>
2018-06-30 13:43:01 +0900

    update dependency

--- build.sbt
+++ build.sbt
@@ -4,8 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.12.6"
 
-resolvers += Resolver.bintrayRepo("smqd", "smqd")
+resolvers += Resolver.sonatypeRepo("public")
 
-libraryDependencies += "t2x.smqd" %% "smqd-core" % "0.2.1"
+libraryDependencies += "com.thing2x" %% "smqd-core" % "0.3.1-SNAPSHOT"
 
 mainClass := Some("sample.helloworld.scala.Main")
--- src/main/scala/sample/helloworld/scala/Main.scala
+++ src/main/scala/sample/helloworld/scala/Main.scala
@@ -1,15 +1,29 @@
+// Copyright 2018 UANGEL
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package sample.helloworld.scala
 
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server._
 import com.typesafe.config.Config
 import spray.json._
-import t2x.smqd.Smqd
+import com.thing2x.smqd.Smqd
 
 /**
   * 2018. 6. 27. - Created by Kwon, Yeong Eon
   */
-object Main extends t2x.smqd.SmqMain {
+object Main extends com.thing2x.smqd.SmqMain {
 
   override val logo: String =
     """
@@ -34,7 +48,7 @@ object Main extends t2x.smqd.SmqMain {
   }
 }
 
-class Controller(name: String, smqd: Smqd, config: Config) extends t2x.smqd.rest.RestController(name, smqd, config) with Directives {
+class Controller(name: String, smqd: Smqd, config: Config) extends com.thing2x.smqd.rest.RestController(name, smqd, config) with Directives {
 
   override val routes: Route = greeting ~ echo
 
